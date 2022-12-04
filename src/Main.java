7be6a6814c954666537b2adde02c8f19300ca798
bogdanlukancuk <ibogdan533@gmail.com>
2022-12-05 02:31:18 +0300

    fourth

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public
 class Main {
     public
-    static void main(String[] args) throws InterruptedException {
+    static void main(String[] args) {
         Process reversi = new Process();
         reversi.startGame();
     }
